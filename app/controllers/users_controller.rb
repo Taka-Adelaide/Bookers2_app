f4dd75f77a7664b0f10fc1d1b018d0813bd8e977
Taka-Adelaide <takapenang08@gmail.com>
2021-10-23 09:57:51 +0000

    InitialCommit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = current_user
+    @books = current_user.book
+  end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user) .permit(:name, :profile_image, :introduction)
+  end
+end
